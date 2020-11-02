d7a5cfd4a97de84a4e3eacfd36686cb398983e6b
lucas gabriel de souza <lucassouza.ads@hotmail.com>
2020-11-02 01:07:57 -0300

    Estudando XML em Windows Form

---
+++ EstudoXML/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace EstudoXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lbxEmpresas = new System.Windows.Forms.ListBox();
+            this.lblTituloPessoa = new System.Windows.Forms.Label();
+            this.lbxPessoas = new System.Windows.Forms.ListBox();
+            this.lblTituloEmpresa = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblTitulo.Location = new System.Drawing.Point(201, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(235, 31);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Agenda de Contatos";
+            // 
+            // lbxEmpresas
+            // 
+            this.lbxEmpresas.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbxEmpresas.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbxEmpresas.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.lbxEmpresas.FormattingEnabled = true;
+            this.lbxEmpresas.ItemHeight = 15;
+            this.lbxEmpresas.Location = new System.Drawing.Point(12, 114);
+            this.lbxEmpresas.Name = "lbxEmpresas";
+            this.lbxEmpresas.Size = new System.Drawing.Size(608, 90);
+            this.lbxEmpresas.TabIndex = 1;
+            // 
+            // lblTituloPessoa
+            // 
+            this.lblTituloPessoa.AutoSize = true;
+            this.lblTituloPessoa.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTituloPessoa.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblTituloPessoa.Location = new System.Drawing.Point(3, 222);
+            this.lblTituloPessoa.Name = "lblTituloPessoa";
+            this.lblTituloPessoa.Size = new System.Drawing.Size(235, 31);
+            this.lblTituloPessoa.TabIndex = 2;
+            this.lblTituloPessoa.Text = "Contatos de Pessoas";
+            // 
+            // lbxPessoas
+            // 
+            this.lbxPessoas.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbxPessoas.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbxPessoas.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.lbxPessoas.FormattingEnabled = true;
+            this.lbxPessoas.ItemHeight = 15;
+            this.lbxPessoas.Location = new System.Drawing.Point(12, 256);
+            this.lbxPessoas.Name = "lbxPessoas";
+            this.lbxPessoas.Size = new System.Drawing.Size(608, 90);
+            this.lbxPessoas.TabIndex = 3;
+            // 
+            // lblTituloEmpresa
+            // 
+            this.lblTituloEmpresa.AutoSize = true;
+            this.lblTituloEmpresa.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTituloEmpresa.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblTituloEmpresa.Location = new System.Drawing.Point(3, 80);
+            this.lblTituloEmpresa.Name = "lblTituloEmpresa";
+            this.lblTituloEmpresa.Size = new System.Drawing.Size(256, 31);
+            this.lblTituloEmpresa.TabIndex = 4;
+            this.lblTituloEmpresa.Text = "Contatos de Empresas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(633, 419);
+            this.Controls.Add(this.lblTituloEmpresa);
+            this.Controls.Add(this.lbxPessoas);
+            this.Controls.Add(this.lblTituloPessoa);
+            this.Controls.Add(this.lbxEmpresas);
+            this.Controls.Add(this.lblTitulo);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Agenda";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.ListBox lbxEmpresas;
+        private System.Windows.Forms.Label lblTituloPessoa;
+        private System.Windows.Forms.ListBox lbxPessoas;
+        private System.Windows.Forms.Label lblTituloEmpresa;
+    }
+}
+
---
+++ EstudoXML/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace EstudoXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTitulo.Text = CarregarTituloAgenda();
+            CarregarListaEmpresa();
+            CarregarListaPessoa();
+        }
+
+        private string CarregarTituloAgenda()
+        {
+            XmlDocument documentoXml = new XmlDocument();
+            documentoXml.Load(@"C:\Users\loja\Source\Repos\LucasGDS1\CursoCSharpIntermediario\EstudoXML\Agenda.xml");
+            XmlNode noTitulo = documentoXml.SelectSingleNode("/agenda/titulo");
+            return noTitulo.InnerText;
+        }
+
+        private void CarregarListaEmpresa()
+        {
+            XmlDocument documentoXml = new XmlDocument();
+            documentoXml.Load(@"C:\Users\loja\Source\Repos\LucasGDS1\CursoCSharpIntermediario\EstudoXML\Agenda.xml");
+            XmlNodeList noLista = documentoXml.SelectNodes("/agenda/contatos/empresas/empresa");
+            foreach(XmlNode item in noLista)
+            {
+                string id = item.Attributes["id"].Value;
+                string nome = item.Attributes["name"].Value;
+                string dataDeAbertura = item.Attributes["dataAbertura"].Value;
+                lbxEmpresas.Items.Add(ToString(id, nome, dataDeAbertura));
+            }
+        }
+
+        private void CarregarListaPessoa()
+        {
+            XmlDocument documentoXml = new XmlDocument();
+            documentoXml.Load(@"C:\Users\loja\Source\Repos\LucasGDS1\CursoCSharpIntermediario\EstudoXML\Agenda.xml");
+            XmlNodeList noLista = documentoXml.SelectNodes("/agenda/contatos/pessoas/pessoa");
+            foreach (XmlNode item in noLista)
+            {
+                string id = item.Attributes["id"].Value;
+                string nome = item.Attributes["name"].Value;
+                string idade = item.Attributes["idade"].Value;
+                lbxPessoas.Items.Add(ToString(id,nome,idade));
+            }
+        }
+
+        private string ToString(string value1, string value2, string value3)
+        {
+            return value1 + ", " + value2 + ", " + value3;
+        }
+    }
+}
