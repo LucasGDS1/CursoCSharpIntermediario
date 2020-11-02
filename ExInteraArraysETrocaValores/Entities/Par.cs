98db3cf2def0fbfd7013c8a87beb3b9d1ba95883
lucas gabriel de souza <lucassouza.ads@hotmail.com>
2020-11-01 22:09:01 -0300

    Exercicio para interação e troca dos valores.

---
+++ ExInteraArraysETrocaValores/Entities/Par.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExInteraArraysETrocaValores.Entities
+{
+    public class Par<T, U>
+    {
+        public T Var1 { get; set; }
+        public U Var2 { get; set; }
+
+        public Par(T var1, U var2)
+        {
+            this.Var1 = var1;
+            this.Var2 = var2;
+        }
+
+        public override String ToString()
+        {
+            return "(" + Var1 + ", " + Var2 + ")";
+        }
+    }
+}
---
+++ ExInteraArraysETrocaValores/Program.cs
@@ -0,0 +1,43 @@
+﻿using ExInteraArraysETrocaValores.Entities;
+using System;
+using System.Linq;
+
+namespace ExInteraArraysETrocaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var meuPar = new Par<int, string>[]
+            {
+                new Par<int, string>(1,"b"),
+                new Par<int, string>(3,"b"),
+                new Par<int, string>(5,"a")
+            };
+            
+            InteraArrays(meuPar);
+
+            var novoObjeto = new Par<int, int>(15, 30);
+            
+            TrocaValores(novoObjeto);
+            Console.WriteLine(novoObjeto);
+        }
+        
+        //Se o método for genérico os Tipos dos parâmetros também devem ser genéricos
+        private static void InteraArrays<T, U>(Par<T, U>[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void TrocaValores<T>(Par<T,T> array)
+        {
+            T aux;
+            aux = array.Var1;
+            array.Var1 = array.Var2;
+            array.Var2 = aux;
+        }
+    }
+}
