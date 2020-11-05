12e3f2c0971db1dbd3ea8470864fa3d68a544fdc
lucas gabriel de souza <lucassouza.ads@hotmail.com>
2020-11-05 01:09:00 -0300

    Implementado método de criação de elementos e verificação.

--- EstudoXML/Form1.cs
+++ EstudoXML/Form1.cs
@@ -21,6 +21,7 @@ namespace EstudoXML
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            CriaNovosElementos();
             lblTitulo.Text = CarregarTituloAgenda();
             CarregarListaEmpresa();
             CarregarListaPessoa();
@@ -39,7 +40,7 @@ namespace EstudoXML
             XmlDocument documentoXml = new XmlDocument();
             documentoXml.Load(@"C:\Users\loja\Source\Repos\LucasGDS1\CursoCSharpIntermediario\EstudoXML\Agenda.xml");
             XmlNodeList noLista = documentoXml.SelectNodes("/agenda/contatos/empresas/empresa");
-            foreach(XmlNode item in noLista)
+            foreach (XmlNode item in noLista)
             {
                 string id = item.Attributes["id"].Value;
                 string nome = item.Attributes["name"].Value;
@@ -58,10 +59,64 @@ namespace EstudoXML
                 string id = item.Attributes["id"].Value;
                 string nome = item.Attributes["name"].Value;
                 string idade = item.Attributes["idade"].Value;
-                lbxPessoas.Items.Add(ToString(id,nome,idade));
+                lbxPessoas.Items.Add(ToString(id, nome, idade));
             }
         }
 
+        private void CriaNovosElementos()
+        {
+            XmlDocument documentXml = new XmlDocument();
+            documentXml.Load(@"C:\Users\loja\Source\Repos\LucasGDS1\CursoCSharpIntermediario\EstudoXML\Agenda.xml");
+
+
+            XmlAttribute id = documentXml.CreateAttribute("id");
+            id.Value = "4";
+            XmlAttribute name = documentXml.CreateAttribute("name");
+            name.Value = "Lucas Gabriel";
+            XmlAttribute idade = documentXml.CreateAttribute("idade");
+            idade.Value = "22";
+
+            if (Existe(id.Value))
+            {
+                MessageBox.Show($"O {name.Value} já existe na lista de contatos, por favor informe dados diferentes!");
+            } else
+            {
+                XmlNode pessoa = documentXml.CreateElement("pessoa");//Create element é a tag do XML
+                pessoa.Attributes.Append(id);
+                pessoa.Attributes.Append(name);
+                pessoa.Attributes.Append(idade);
+
+                XmlNode pessoas = documentXml.SelectSingleNode("/agenda/contatos/pessoas");//Leitura do Nó onde serão adicionados os nós filhos
+                pessoas.AppendChild(pessoa);
+
+                documentXml.Save(@"C:\Users\loja\Source\Repos\LucasGDS1\CursoCSharpIntermediario\EstudoXML\Agenda.xml");
+            }
+
+
+        }
+
+        private bool Existe(string id)
+        {
+            var existe = false;
+
+            XmlDocument documentXml = new XmlDocument();
+            documentXml.Load(@"C:\Users\loja\Source\Repos\LucasGDS1\CursoCSharpIntermediario\EstudoXML\Agenda.xml");
+
+            XmlNodeList listaContatos = documentXml.SelectNodes("/agenda/contatos/pessoas/pessoa");
+
+            foreach (XmlNode item in listaContatos)
+            {
+                if (id.Equals(item.Attributes["id"].Value))
+                {
+                    existe = true;
+                }
+                else
+                {
+                    existe = false;
+                }
+            }
+            return existe;
+        }
         private string ToString(string value1, string value2, string value3)
         {
             return value1 + ", " + value2 + ", " + value3;
